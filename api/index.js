00243fa7250ff33fa8937ab4400aa7f9350cb808
VanTXDev <vantx2103@gmail.com>
2023-04-13 09:52:46 +0700

    Creating function handle register

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
+app.use(
+	cors({
+		credentials: true,
+		origin: "http://localhost:5173"
+	})
+);
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+	const { email, phoneNumber, name, password, repassword } = req.body;
+	console.log(name + email + password + phoneNumber + repassword);
+	res.send("Register success");
 });
 
 app.listen(PORT, () => {
